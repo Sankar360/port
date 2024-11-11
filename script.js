9cadf780c7e1035399f177b84193aa65b55ace3a
sankar360 <sivaram1712000@gmail.com>
2024-11-11 19:48:40 +0530

    new changes added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var typed=new Typed(".text",{
-        strings:["Frontend Developer","Web Developer","ReactJs Developer"],
+        strings:["Frontend Developer","ReactJs Developer","Fullstack Developer"],
         typeSpeed:100,
         backSpeed:100,
         backDelay:1000,
